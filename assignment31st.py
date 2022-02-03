3c527ea0b6951c4ebbe7c4c35da51d93a8542f9b
leletiaudrey <leletiaudrey@gmail.com>
2022-02-03 19:28:07 +0300

    my assignment

---
+++ assignment31st.py
@@ -0,0 +1,19 @@
+a=170 
+b=172
+c=150
+d=("John""170cm")
+e=("Alice 172cm")
+f=("Bob 150cm")
+print(d)
+print(e)
+print(f)
+if a>b:
+    print("John is the tallest")
+if a>c>b:
+    print("John is the tallest")
+if b>c:
+    print("Alice is the tallest")
+else:
+    print("Bob is the tallest")
+
+
